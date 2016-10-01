4660817b410ea3cc07a2a758bc6bb70e8a54a1ec
josivanSilvaCodes <josivan.engenharia@gmail.com>
2016-09-30 21:11:03 -0300

    Delete Sound.java

--- Sound.java
+++
@@ -1,8 +0,0 @@
-import java.applet.Applet;
-import java.applet.AudioClip;
-
-public class Sound {
-	public static final AudioClip BALL = Applet.newAudioClip(Sound.class.getResource("sound_hit.wav"));
-	public static final AudioClip GAMEOVER = Applet.newAudioClip(Sound.class.getResource("sound_gameover.wav"));
-	public static final AudioClip BACK = Applet.newAudioClip(Sound.class.getResource("music_1.wav"));
-}
